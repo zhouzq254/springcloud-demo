a555f6db2ba64eb66c75702bb40218bda609a772
zhouzq254 <929562100@qq.com>
2018-04-16 22:17:20 +0800

    注释

--- springcloud/springRibbon/src/main/java/com/scnu/ribbon/controller/ConsumerController.java
+++ springcloud/springRibbon/src/main/java/com/scnu/ribbon/controller/ConsumerController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * 
+ * @author zzq
+ *
+ */
 @RestController
 public class ConsumerController {
 	
