42dde40a9a7d19a048b502e1ea7909782108d39a
zhouzq254 <929562100@qq.com>
2018-04-16 22:08:28 +0800

    添加日志

--- springcloud/springRibbon/src/main/java/com/scnu/ribbon/controller/ConsumerController.java
+++ springcloud/springRibbon/src/main/java/com/scnu/ribbon/controller/ConsumerController.java
@@ -1,6 +1,7 @@
 package com.scnu.ribbon.controller;
 
 import org.apache.commons.lang.math.RandomUtils;
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,12 +10,15 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
+	
+	private Logger logger = Logger.getLogger(ConsumerController.class);
 	@Autowired
     RestTemplate restTemplate;
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String add() {
     	int a = RandomUtils.nextInt(100);
     	int b = RandomUtils.nextInt(50);
+    	logger.info("a="+a + ",b="+b);
         return restTemplate.getForEntity("http://compute-service/add?a=" + a + "&b=" + b , String.class).getBody();
     }
 }
